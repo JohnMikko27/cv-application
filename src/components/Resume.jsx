9d4d2f62de65398e1ef5940585bb4c4d4233f6e9
Mikko <realmiks27@gmail.com>
2024-01-16 11:43:54 -0800

    Remove placeholder text

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,34 +7,33 @@ const Resume = ({value}) => {
       {/* <Section values={[value.fullName, value.phoneNumber, value.email, value.address]}></Section> */}
 
       <div className="personalInformation">
-        <div>John Mikko Velasquez{value.fullName}</div>
-        <div>999-999-9999{value.phoneNumber}</div>
-        <div>google.com@gmail.com{value.email}</div>
-        <div>1234 Star Avenue{value.address}</div>
+        <div>{value.fullName}</div>
+        <div>{value.phoneNumber}</div>
+        <div>{value.email}</div>
+        <div>{value.address}</div>
       </div>
       <div className="education">
         <div>
-          <div>San Francisco State University{value.schoolName}</div>
-          <div>Computer Science{value.degree}</div>
+          <div>{value.schoolName}</div>
+          <div>{value.degree}</div>
         </div>
         <div>
-          <div>January 2024 - May 2026{value.schoolStartDate} - {value.schoolEndDate}</div>
-          <div>San Francisco, CA{value.schoolLocation}</div>
+          <div>{value.schoolStartDate} - {value.schoolEndDate}</div>
+          <div>{value.schoolLocation}</div>
         </div>
       </div>
       <div className="experience">
         <div>
           <div>
-            <div>Google{value.companyName}</div>
-            <div>Software Engineer{value.positionTitle}</div>
+            <div>{value.companyName}</div>
+            <div>{value.positionTitle}</div>
           </div>
           <div>
-            <div>January 2024 - May 2024{value.experienceStartDate} - {value.experienceEndDate}</div>
-            <div>San Francisco, CA{value.experienceLocation}</div>
+            <div>{value.experienceStartDate} - {value.experienceEndDate}</div>
+            <div>{value.experienceLocation}</div>
           </div>
         </div>
-        <div>Contributed to an open source project that aims to support sustainability and address climate change by providing a free, high-quality, web-based application that displays energy information in a browser
-Wrote unit tests using JavaScript and Mocha framework that parses csv files and ensures that displayed energy information is accurate {value.description}</div>
+        <div>{value.description}</div>
       </div>
     </div>
     
